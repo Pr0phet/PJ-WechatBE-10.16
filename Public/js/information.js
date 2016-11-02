18c8c48a4ba1a79a900f45b09dba3b7c1bb37ee5
ben <895488201@qq.com>
2016-11-03 03:12:41 +0800

    upload information.js

--- Public/js/information.js
+++ Public/js/information.js
@@ -4,7 +4,24 @@
 	if(!id)
 		location.href = "index";
 
-	var data = {};
+	var data = {
+		description: "123",
+		owner: "coderben",
+		owner_pic: "/EXbook/Public/img/touxiang.jpg",
+		pic: [{
+			url: "/EXbook/Public/img/touxiang.jpg",
+		}, {
+			url: "/EXbook/Public/img/touxiang.jpg",
+		}],
+		price: "123",
+		time: "1小时前",
+		comments: [{
+			time: "17:30",
+			owner: "lalala",
+			description: "tell me why",
+			pic: "/EXbook/Public/img/touxiang.jpg",
+		}],
+	};
 
 	new Vue({
 		el: "#information",
