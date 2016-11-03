a4369d0a804cabc4661993476d9b78caae3de499
ben <895488201@qq.com>
2016-11-03 08:10:55 +0800

    complete 80%

--- Public/js/information.js
+++ Public/js/information.js
@@ -14,6 +14,7 @@
 		dataType: "json",
 		success: function(datas){
 			data = datas;
+			data.comments.reverse();
 			new Vue({
 				el: "#information",
 				data: data,
@@ -24,7 +25,25 @@
 		},
 	});
 
+	var isLogin = false;
+	$.ajax({
+		url: "/EXbook/index.php/Home/Index/checkSession",
+		type: "post",
+		dataType: "json",
+		success: function(datas){
+			if(datas.status ==1)
+				isLogin = true;
+		},
+		error: function(e){
+			tools.alertMessage("访问服务器错误");
+		},
+	});
+
 	$(".putComment").click(function(){
+		if(isLogin == false){
+			tools.alertMassage("没有登录");
+			location.href = "subLogin";
+		}
 		$(".leaders").stop().animate({
 			bottom: -50,
 		}, function(){
@@ -34,6 +53,7 @@
 			.animate({
 				bottom: 0,
 			}).find(".send").click(function(){
+				$(".commentText").val("");
 				$.ajax({
 					url: "/EXbook/index.php/Home/Index/addComment",
 					type: "post",
@@ -44,7 +64,7 @@
 					dataType: "json",
 					success: function(datas){
 						data.comments = data.comments || [],
-						data.comments.push({
+						data.comments.unshift({
 							owner: datas.owner,
 							pic: datas.owner_pic,
 							description: datas.content,
@@ -52,10 +72,19 @@
 						});
 					},
 					error: function(e){
-						tools.alertMessage("连接数据库错误");
+						tools.alertMessage("连接服务器错误");
 					}
 				});
 			});
 		});
+	});
+
+	$(".zu").click(function(){
+		if(isLogin == false){
+			tools.alertMassage("没有登录");
+			location.href = "subLogin";
+		}
+		location.href = "chatroom?id=" + data.ownerid
+			+ "&name=" + data.owner + "&pic=" + data.owner_pic;
 	});
 })(window);
